da94269e26e23cfa8447a24f4b8a70c859999f3a
Gaurav7109 <93638105+Gaurav7109@users.noreply.github.com>
2023-11-24 11:11:20 +0530

    Add files via upload

--- portfolio_census_india_world_queries.sql
+++ portfolio_census_india_world_queries.sql
@@ -44,8 +44,12 @@ select count(*) from world_population;
 
 -- 02 All data for states Uttarakhand & Rajasthan from indian data and data for india from world data
 
-select ggl.district,ip.state,ip.population,ggl.growth,ggl.sex_ratio,ggl.literacy from dataset_growth_gender_ratio_literacy 
-AS ggl inner join dataset_indian_population as ip on ggl.district = ip.District where ip.state in ('Uttarakhand', 'Rajasthan');
+select ggl.district,ip.state,ip.population,ggl.growth,ggl.sex_ratio,ggl.literacy 
+from dataset_growth_gender_ratio_literacy AS ggl 
+inner join 
+dataset_indian_population as ip 
+on ggl.district = ip.District 
+where ip.state in ('Uttarakhand', 'Rajasthan');
 
 select * from world_population where country= "India";
 
@@ -78,9 +82,11 @@ ORDER BY avg_population DESC;
 
 -- 07 Population of India AND world
 
-select sum(population) as Total_Population_as_on_2011 from dataset_indian_population; 
+select sum(population) as Total_Population_as_on_2011 
+from dataset_indian_population; 
 
-select sum(2022_population) as Total_Population_as_on_2022 from world_population; 
+select sum(2022_population) as Total_Population_as_on_2022 
+from world_population; 
 
 
 -- 08 Find the highest and lowest population among all countries in 2022
@@ -127,57 +133,101 @@ select avg(GROWTH_RATE)  as Avg_Growth from world_population;
 
 
 -- 14 avg growth of ALL states 
-select State, avg(Growth)  as Avg_Growth from dataset_growth_gender_ratio_literacy group by state order by Avg_Growth desc;
+select State, avg(Growth) as Avg_Growth 
+from dataset_growth_gender_ratio_literacy 
+group by state 
+order by Avg_Growth desc;
 
 
 -- 15 avg sex ratio of each state:
-select State, round(avg(Sex_Ratio), 0) as Avg_Sex_Ratio from dataset_growth_gender_ratio_literacy group by state order by Avg_Sex_Ratio desc;
+select State, round(avg(Sex_Ratio), 0) as Avg_Sex_Ratio 
+from dataset_growth_gender_ratio_literacy 
+group by state 
+order by Avg_Sex_Ratio desc;
 
 
 -- 16 avg literacy rate of each state and return all states having avg literacy rate greater than 85
-select State, round(avg(Literacy), 0) as Avg_Literacy_rate from dataset_growth_gender_ratio_literacy 
-group by state having round(avg(Literacy), 0) >= 85 order by Avg_Literacy_rate desc;
+select State, round(avg(Literacy), 0) as Avg_Literacy_rate 
+from dataset_growth_gender_ratio_literacy 
+group by state 
+having round(avg(Literacy), 0) >= 85 
+order by Avg_Literacy_rate desc;
 
 
 -- 17 Top 3 states showing highest growth rate and top three countries having highest growth rate
 
-select state, avg(growth) * 100 as Avg_growth from dataset_growth_gender_ratio_literacy group by state order by Avg_growth desc limit 3;
+select state, avg(growth) * 100 as Avg_growth 
+from dataset_growth_gender_ratio_literacy 
+group by state 
+order by Avg_growth desc 
+limit 3;
 
-select country, growth_rate from world_population order by growth_rate desc limit 3;
+select country, growth_rate 
+from world_population 
+order by growth_rate desc 
+limit 3;
 
 
 -- 18 5 states showing highest and Lowest sex ratio:
 
-select state, round(avg(Sex_ratio), 2) as Avg_Sex_Ratio from dataset_growth_gender_ratio_literacy group by state order by Avg_Sex_Ratio desc limit 5;
+select state, round(avg(Sex_ratio), 2) as Avg_Sex_Ratio 
+from dataset_growth_gender_ratio_literacy 
+group by state 
+order by Avg_Sex_Ratio desc 
+limit 5;
 
-select state, round(avg(Sex_ratio), 2) as Avg_Sex_Ratio from dataset_growth_gender_ratio_literacy group by state order by Avg_Sex_Ratio asc limit 5;
+select state, round(avg(Sex_ratio), 2) as Avg_Sex_Ratio 
+from dataset_growth_gender_ratio_literacy 
+group by state 
+order by Avg_Sex_Ratio asc 
+limit 5;
 
 
 -- 19 top and bottom 3 states in literacy state:
 
-select state, round(avg(Literacy), 0) as Avg_Literacy from dataset_growth_gender_ratio_literacy group by state order by Avg_Literacy desc limit 3;
+select state, round(avg(Literacy), 0) as Avg_Literacy 
+from dataset_growth_gender_ratio_literacy 
+group by state 
+order by Avg_Literacy desc 
+limit 3;
 
-select state, round(Avg(Literacy), 0) as Avg_Literacy from dataset_growth_gender_ratio_literacy group by state order by Avg_Literacy asc limit 3;
+select state, round(Avg(Literacy), 0) as Avg_Literacy 
+from dataset_growth_gender_ratio_literacy 
+group by state 
+order by Avg_Literacy asc 
+limit 3;
 
 
 -- 20 Both Top and Bottom States in single table with top 3 entries as top states and bottom 3 as bottom states in literacy:
 
 select * from 
 (select * from (
-select state, round(avg(Literacy), 0) as Avg_Literacy from dataset_growth_gender_ratio_literacy group by state order by Avg_Literacy desc limit 3)
+select state, round(avg(Literacy), 0) as Avg_Literacy 
+from dataset_growth_gender_ratio_literacy 
+group by state 
+order by Avg_Literacy desc 
+limit 3)
 a
 union
 select * from (
-select state, round(avg(Literacy), 0) as Avg_Literacy from dataset_growth_gender_ratio_literacy group by state order by Avg_Literacy limit 3)
+select state, round(avg(Literacy), 0) as Avg_Literacy 
+from dataset_growth_gender_ratio_literacy 
+group by state 
+order by Avg_Literacy 
+limit 3)
  b
 ) c;
 
 
 -- 21 Find all districts of States where state's name is starting with letter 'a' & 'b' and find all countries with capital where country name is starting with 'a' or 'b';
 
-select district,state from dataset_growth_gender_ratio_literacy where Lower(State) like "a%" or Lcase(State) like "b%";
+select district,state 
+from dataset_growth_gender_ratio_literacy 
+where Lower(State) like "a%" or Lcase(State) like "b%";
 
-select country,capital_city from world_population where Lower(country) like "a%" or Lcase(country) like "b%";
+select country,capital_city 
+from world_population 
+where Lower(country) like "a%" or Lcase(country) like "b%";
 
 
 -- 22 Find the countries whose total population is greater than the average population
@@ -257,57 +307,79 @@ LIMIT 100;
 
 -- 31 Total males and females in each states
 
-select d.state, sum(d.males) as total_males, sum(d.females) as total_females from
-(
-select c.district, c.state, round(c.population/(c.sex_ratio+1), 0) as Males, round((c.population*c.sex_ratio)/(c.sex_ratio+1),0) as females from
-(select d1.district, d1.state, d1.sex_ratio/1000 as sex_ratio, d2.Population from dataset_growth_gender_ratio_literacy as d1
-join dataset_indian_population as d2 on d1.District = d2.District order by Population desc) c
+select d.state, sum(d.males) as total_males, sum(d.females) as total_females 
+from (
+select c.district, c.state, round(c.population/(c.sex_ratio+1), 0) as Males, round((c.population*c.sex_ratio)/(c.sex_ratio+1),0) as females 
+from (
+select d1.district, d1.state, d1.sex_ratio/1000 as sex_ratio, d2.Population 
+from dataset_growth_gender_ratio_literacy as d1
+join dataset_indian_population as d2 
+on d1.District = d2.District 
+order by Population desc) c
 ) d
 group by d.state;
 
 
 -- 32 Total litracy rate of each state
 
-with literacy_rate_of_each_state as (select state, 100*sum(literate_people)/(sum(illiterate_people)+sum(literate_people)) as literacy_rate from
-(
-select district, state, round(literacy_ratio*Population, 0) as literate_people, round((1 - literacy_ratio)* population, 0) as illiterate_people from
-(select d1.district, d1.state, d1.literacy/100 as literacy_ratio, d2.Population from dataset_growth_gender_ratio_literacy as d1
-join dataset_indian_population as d2 on d1.District = d2.District order by Population desc) c) x
-group by state order by literacy_rate desc
+with literacy_rate_of_each_state as (select state, 100*sum(literate_people)/(sum(illiterate_people)+sum(literate_people)) as literacy_rate 
+from (
+select district, state, round(literacy_ratio*Population, 0) as literate_people, round((1 - literacy_ratio)* population, 0) as illiterate_people 
+from (
+select d1.district, d1.state, d1.literacy/100 as literacy_ratio, d2.Population 
+from dataset_growth_gender_ratio_literacy as d1
+join dataset_indian_population as d2 
+on d1.District = d2.District
+ order by Population desc) c) x
+group by state 
+order by literacy_rate desc
 )
 select * from literacy_rate_of_each_state;
 
 
 -- 33 Total litracy rate of India
 
-select (sum(literate_people)/((sum(literate_people))+(sum(illiterate_people))))*100 as literacy_rate_of_country from 
-(select state, sum(literate_people) as literate_people, sum(illiterate_people) as illiterate_people from
-(
-select district, state, round(literacy_ratio*Population, 0) as literate_people, round((1 - literacy_ratio)* population, 0) as illiterate_people from
-(select d1.district, d1.state, d1.literacy/100 as literacy_ratio, d2.Population from dataset_growth_gender_ratio_literacy as d1
-join dataset_indian_population as d2 on d1.District = d2.District order by Population desc) c) x
-group by state order by literate_people desc, illiterate_people desc
+select (sum(literate_people)/((sum(literate_people))+(sum(illiterate_people))))*100 as literacy_rate_of_country 
+from (
+select state, sum(literate_people) as literate_people, sum(illiterate_people) as illiterate_people 
+from (
+select district, state, round(literacy_ratio*Population, 0) as literate_people, round((1 - literacy_ratio)* population, 0) as illiterate_people 
+from (
+select d1.district, d1.state, d1.literacy/100 as literacy_ratio, d2.Population 
+from dataset_growth_gender_ratio_literacy as d1
+join dataset_indian_population as d2 
+on d1.District = d2.District 
+order by Population desc) c) x
+group by state 
+order by literate_people desc, illiterate_people desc
 )d;
 
 
 -- 34 total_populatioin of India in previous census 
 
 select Sum(Previous_Census_Population) as Previous_Census_Population, Sum(Current_Census_Population) as Current_Census_Population
-from
-(
-select State, Sum(Previous_Census_Population) as Previous_Census_Population, Sum(Current_Census_Population) as Current_Census_Population from
-(
-select district, state, round(population/(1+growth),0) as Previous_Census_Population, Population as Current_Census_Population from
-(select d1.district, d1.state, d1.growth as growth, d2.Population from dataset_growth_gender_ratio_literacy as d1
-join dataset_indian_population as d2 on d1.District = d2.District order by Population desc) c) x
-group by State order by Previous_Census_Population desc, Current_Census_Population desc) y;
+from (
+select State, Sum(Previous_Census_Population) as Previous_Census_Population, Sum(Current_Census_Population) as Current_Census_Population 
+from(
+select district, state, round(population/(1+growth),0) as Previous_Census_Population, Population as Current_Census_Population 
+from (
+select d1.district, d1.state, d1.growth as growth, d2.Population 
+from dataset_growth_gender_ratio_literacy as d1
+join dataset_indian_population as d2 
+on d1.District = d2.District 
+order by Population desc) c) x
+group by State 
+order by Previous_Census_Population desc, Current_Census_Population desc) y;
 
 
 -- 35 output top 3 district from each state with highest literacy rate.
 
-select District, State, Literacy, rnk from 
-(select District, State, Literacy, rank() over(partition by State order by Literacy desc) rnk from dataset_growth_gender_ratio_literacy)x
-where rnk in (1, 2, 3) order by State;
+select District, State, Literacy, rnk 
+from (
+select District, State, Literacy, rank() over(partition by State order by Literacy desc) rnk 
+from dataset_growth_gender_ratio_literacy)x
+where rnk in (1, 2, 3) 
+order by State;
 
 
 
